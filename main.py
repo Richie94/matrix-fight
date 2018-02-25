2a06da2492ff182994e791f8c3af52e0a693eeaa
Richie94 <richard.kwasnicki@gmx.de>
2018-02-25 13:35:52 +0100

    added 2 strategys

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class MatrixFight():
 		self.width = width
 		self.groups = list(range(1, groups+1, 1))
 
+		self.strategys = {1:"m", 2:"r", 3:"h"}
+
 		self.rounds = 0
 
 		self.createMatrix()
@@ -28,13 +30,26 @@ class MatrixFight():
 			self.strenghts[x, y] = 1
 
 	def printMatrix(self):
+		fields = {i : 0 for i in self.groups+ [0]}
+		army = {i : 0 for i in self.groups+ [0]}
 		self.screen.clear()
 		for idx, row in enumerate(self.matrix):
 			#line = ""
 			for idx2, v in enumerate(row):
-				line = "{}-{} ".format(v, self.strenghts[idx,idx2])
+				armyStrength = self.strenghts[idx,idx2]
+				line = "{}   ".format(armyStrength)
+				fields[v] += 1
+				army[v] += armyStrength
 
 				self.screen.addstr(idx, idx2*4, line, curses.color_pair(v*20))
+		fieldStr, armyStr = "Fields: ", "Army: "
+		for f in sorted(fields):
+			fieldStr += "{}: {} ".format(f, fields[f])
+		for f in sorted(army):
+			armyStr += "{}: {} ".format(f, army[f])
+
+		self.screen.addstr(11,0, fieldStr)
+		self.screen.addstr(12, 0, armyStr)
 		self.screen.refresh()
 
 	def getNeighbours(self, x, y):
@@ -61,22 +76,25 @@ class MatrixFight():
 			winner = p1
 		else:
 			# calculate chance for s1 to win
-			winPercentage = min(max((s1-s2)*0.2, 0), 1)
+			winPercentage = min(max((s1-s2)*0.1+0.5, 0), 1)
 			diceRoll = np.random.random_integers(10)
 			if diceRoll <= winPercentage * 10:
 				winner = p1
 			else:
 				winner = p2
+			#self.screen.addstr(15,0, "S1 {} S2{} Perc {} Dice {}".format(s1, s2, winPercentage, diceRoll))
+			#self.screen.refresh()
+			#time.sleep(2)
 		return winner
 
-	def nextRound(self):
+	def nextRound(self, speed=0.005):
 		for group in self.groups:
 			self.executeTurn(group)
 			self.printMatrix()
-			time.sleep(.2)
+			time.sleep(speed)
 
 	def executeTurn(self, group):
-		attacker, defender = self.randomTurn(group)
+		attacker, defender = self.chooseTurn(group)
 
 		if attacker is not None and defender is not None:
 			winner = self.fight(attacker, defender)
@@ -87,23 +105,80 @@ class MatrixFight():
 			else:
 				self.strenghts[attacker] = max(self.strenghts[attacker] - 1, 1)
 
+	def chooseTurn(self, group):
+		if self.strategys[group] == "r":
+			return self.randomTurn(group)
+		elif self.strategys[group] == "h":
+			return self.highLowTurn(group)
+		else:
+			return self.maxArmyTurn(group)
+
+
+	def possibleActions(self, group):
+		posAttacker = [tuple(x) for x in np.argwhere(self.matrix == group)]
+		posTargets = {}
+		if len(posAttacker) > 0:
+			posTargets = {x: self.getNeighbours(*x) for x in posAttacker}
+			# delete all neighbours from same group
+			posTargets = {n: [x for x in posTargets[n] if self.matrix[x] != group] for n in
+			              posTargets}
+
+			for n in list(posTargets):
+				if posTargets[n] == []:
+					posTargets.pop(n)
+		return posTargets
 
 	def randomTurn(self, group):
 		attacker, defender = None, None
-		groupMembers = [tuple(x) for x in np.argwhere(self.matrix==group)]
-		if len(groupMembers) > 0:
-			memberNeighbours = {x : self.getNeighbours(*x) for x in groupMembers}
-			# delete all neighbours from same group
-			memberNeighbours = {n : [x for x in memberNeighbours[n] if self.matrix[x] != group] for n in memberNeighbours}
-			# choose members with existing border
-			outerMembers = [idx for idx in groupMembers if len(memberNeighbours[idx]) > 0]
-			chosenId = np.random.choice(len(outerMembers))
-			attacker = tuple(outerMembers[chosenId])
+		posTargets = self.possibleActions(group)
+		if len(posTargets) > 0:
+			# choose random attacker with possible targets
+			posAttackers = [x for x in posTargets if len(posTargets[x]) > 0]
+			chosenId = np.random.choice(len(posAttackers))
+			attacker = tuple(posAttackers[chosenId])
 
-			# 2. let it choose a random neighbour from other group
-			defender = memberNeighbours[attacker][np.random.choice(len(memberNeighbours[attacker]))]
+			# let it choose a random target
+			defender = posTargets[attacker][np.random.choice(len(posTargets[attacker]))]
 
 		return attacker, defender
+
+	def highLowTurn(self, group):
+		attacker, defender = None, None
+		posTargets = self.possibleActions(group)
+		if len(posTargets) > 0:
+			# choose attacker with best army
+			posAttackers = [(x, self.strenghts[x]) for x in posTargets if len(posTargets[x]) > 0]
+			attacker = tuple(sorted(posAttackers, key=lambda x:x[1])[-1][0])
+			#attacker = tuple(posAttackers[chosenId])
+
+			# let it choose a target with lowest army
+			posDefenders = [(x,self.strenghts[x]) for x in posTargets[attacker]]
+			defender = tuple(sorted(posDefenders, key=lambda x: x[1])[0][0])
+			#defender = posTargets[attacker][chosenId]
+
+		return attacker, defender
+
+	def maxArmyTurn(self, group):
+		attacker, defender = None, None
+		posTargets = self.possibleActions(group)
+		if len(posTargets) > 0:
+			# choose attacker with best army
+			posAttackers = [(x, self.strenghts[x]) for x in posTargets if len(posTargets[x]) > 0 and self.strenghts[x] < 9]
+			# if all are maxed
+			if len(posAttackers) == 0:
+				posAttackers = [(x, self.strenghts[x]) for x in posTargets if
+				                len(posTargets[x]) > 0]
+
+			attacker = tuple(sorted(posAttackers, key=lambda x: x[1])[-1][0])
+			# attacker = tuple(posAttackers[chosenId])
+
+			# let it choose a target with lowest army
+			posDefenders = [(x, self.strenghts[x]) for x in posTargets[attacker]]
+			defender = tuple(sorted(posDefenders, key=lambda x: x[1])[0][0])
+		# defender = posTargets[attacker][chosenId]
+
+		return attacker, defender
+
 
 
 def main(stdscr=False):
@@ -114,11 +189,9 @@ def main(stdscr=False):
 		curses.init_pair(i + 1, i, -1)
 
 	env = MatrixFight(stdscr)
-	for i in range(2000):
+	for i in range(20000):
 		env.nextRound()
-		#time.sleep(1)
 
 
 if __name__ == "__main__":
 	curses.wrapper(main)
-	#main()
